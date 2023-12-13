640497208101d92f26d9ae5baedc629d0e0f2e76
basiliskv <subarickv4.1@gmail.com>
2023-12-13 09:46:54 +0900

    fix listener

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -1,14 +1,19 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int16, String  # String型を追加
+from std_msgs.msg import Int16, String
 
 def cb(msg):
-    global node
-    node.get_logger().info("Listen: %d, Time: %s" % (msg.data, time_msg.data))
+    global node, time_msg
+
+    # メッセージが正しく受信されている場合のみログを表示
+    if hasattr(node, 'time_msg'):
+        node.get_logger().info("Listen: %d, Time: %s" % (msg.data, node.time_msg.data))
+    else:
+        node.get_logger().warn("Received countup message, but time_msg is not set yet.")
 
 rclpy.init()
 node = Node("listener")
 pub = node.create_subscription(Int16, "countup", cb, 10)
-time_pub = node.create_subscription(String, "current_time", lambda msg: setattr(node, 'time_msg', msg), 10)  # 追加
+time_pub = node.create_subscription(String, "current_time", lambda msg: setattr(node, 'time_msg', msg), 10)
 
 rclpy.spin(node)
